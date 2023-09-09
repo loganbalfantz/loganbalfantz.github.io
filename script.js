d2a8a0f95ccf3b551acd7080a2728e3c0648ca54
loganbalfantz <41041942+loganbalfantz@users.noreply.github.com>
2023-09-08 20:22:12 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,4 @@
-// Wait for the document to load
+// For the carousel in Media & Engagements
 document.addEventListener("DOMContentLoaded", function() {
-  // Wait 5 seconds to simulate a "loading" period
-  setTimeout(function() {
-    // Hide the loading screen
-    document.getElementById("loading-screen").style.display = "none";
-
-    // Show the landing page
-    const landingPage = document.getElementById("landing-page");
-    landingPage.classList.remove("hidden");
-    landingPage.style.opacity = 0;
-    
-    // Fade in transition for the landing page
-    setTimeout(function() {
-      landingPage.style.opacity = 1;
-    }, 100); // Wait 100 milliseconds for the "display: block" to take effect
-    
-  }, 5000); // 5000 milliseconds = 5 seconds
+  // Your carousel logic here
 });
